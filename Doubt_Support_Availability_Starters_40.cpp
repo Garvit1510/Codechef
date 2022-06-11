d66ba228a67a526bcc5385df685561e88a4b6acd
Garvit1510 <96440124+Garvit1510@users.noreply.github.com>
2022-06-11 13:16:09 +0530

    Create Doubt_Support_Availability_Starters_40.cpp

---
+++ Doubt_Support_Availability_Starters_40.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int D;
+    cin>>D;
+    if(D<=1600)
+    {
+        cout<<"YES\n";
+    }
+    else
+    {
+        cout<<"NO\n";
+    }
+}
